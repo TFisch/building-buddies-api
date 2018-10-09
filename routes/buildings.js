05f9c9bf6d18f927e203abd5c26e21f6bcba0ee9
Gray Smith <gray.smith00@gmail.com>
2018-10-08 20:35:06 -0600

    Create endpoints for adding and new building and retrieving all buildings, still need validation that building has not already been created.

--- routes/buildings.js
+++ routes/buildings.js
@@ -1,10 +1,36 @@
 const express = require('express');
 
 const router = express.Router();
+const environment = process.env.NODE_ENV || 'development';
+const configuration = require('../knexfile')[environment];
+const database = require('knex')(configuration);
 
 // get all buildings
 router.get('/', (req, res) => {
-  res.send('get all buildings');
+  database('buildings')
+    .select()
+    .then(buildings => res.status(200).json(buildings))
+    .catch(err => res.status(500).json({ err }));
+});
+// create new building
+router.post('/', (req, res) => {
+  const { name, address } = req.body;
+  const newBuilding = { name, address };
+  console.log(newBuilding);
+  const requiredParams = ['name', 'address'];
+
+  requiredParams.map((param) => {
+    if (!newBuilding[param]) {
+      return res.status(422).send({
+        error: `Expected format: { name: <String>, address: <String> }. You're missing a "${param}" property.`,
+      });
+    }
+  });
+
+  database('buildings')
+    .insert(newBuilding, 'id')
+    .then(building => res.status(201).json({ id: building[0] }))
+    .catch(err => res.status(500).json({ err }));
 });
 
 module.exports = router;
