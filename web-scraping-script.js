cb98c4e5b7cf903e4dd4c796bdc47db16e65c1c4
Laura Whitaker <laura.k.whitaker@gmail.com>
2018-10-09 14:20:01 -0600

    Refactor to scrap from multiple pages and update apartmentData

--- web-scraping-script.js
+++ web-scraping-script.js
@@ -1,28 +1,46 @@
 const Nightmare = require('nightmare');
-const nightmare = Nightmare( {show: true} );
 const fs = require('fs');
+const urls = [];
 
-nightmare
-  .goto('https://www.apartmentlist.com/co/denver')
-  .evaluate(() => {
-    const apartmentCards = Array.from(document.querySelectorAll('.listing-details'));
-    const apartmentData = apartmentCards.map((apartmentCard) => {
-      let name = apartmentCard.querySelector('.listing-name').innerText;
-      let address = apartmentCard.querySelector('.listing-contact-info').innerText;
-      return {name, address};
-    });
-    return apartmentData;
-  })
-  .end()
-  .then(result => {
-    fs.writeFile('apartmentData.json', JSON.stringify(result, null, "\t"), (error) => {
-      if (error) {
-        console.log('Somthing went wrong:', error)
-      }
-    });
-    console.log('The file has been saved!')
-  })
-  .catch(error => {
-    console.log('Somthing went wrong:', error)
-  })
-  
+for (i = 1; i < 11; i++) {
+  urls.push(`https://www.apartmentlist.com/co/denver/page-${i}`)
+}
+
+const getApartmentData = async url => {
+  try {
+    const nightmare = Nightmare( {show: true} );
+    const result = await nightmare
+      .goto(url)
+      .evaluate(() => {
+        const apartmentCards = Array.from(document.querySelectorAll('.listing-details'));
+        const apartmentData = apartmentCards.map((apartmentCard) => {
+          let name = apartmentCard.querySelector('.listing-name').innerText;
+          let address = apartmentCard.querySelector('.listing-contact-info').innerText;
+          return {name, address};
+        });
+        return apartmentData;
+      })
+      .end();
+
+      return result;
+  } catch(error) {
+    console.error(error);
+    return undefined;
+  }
+}
+
+const finalData = urls.reduce( async (queue, url) => {
+  let dataArray = await queue;
+  const newData = await getApartmentData(url);
+  dataArray = [...dataArray, ...newData];
+  return dataArray;
+}, Promise.resolve([]));
+
+finalData.then(data => {
+  fs.writeFile('apartmentData.json', JSON.stringify(data, null, "\t"), (error) => {
+    if (error) {
+      console.log('Somthing went wrong:', error)
+    }
+  });
+  console.log('The file has been saved!')
+})
