53805ab94bff1bfffcd806e7e85148e331dd7263
Laura Whitaker <laura.k.whitaker@gmail.com>
2018-10-08 20:38:33 -0600

    Add data to file

--- web-scraping-script.js
+++ web-scraping-script.js
@@ -1,20 +1,26 @@
 const Nightmare = require('nightmare');
 const nightmare = Nightmare( {show: true} );
+const fs = require('fs');
 
 nightmare
   .goto('https://www.apartmentlist.com/co/denver')
   .evaluate(() => {
-    const appartmentCards = Array.from(document.querySelectorAll('.listing-details'));
-    const appartmentData = appartmentCards.map((appartmentCard) => {
-      let name = appartmentCard.querySelector('.listing-name').innerText;
-      let address = appartmentCard.querySelector('.listing-contact-info').innerText;
+    const apartmentCards = Array.from(document.querySelectorAll('.listing-details'));
+    const apartmentData = apartmentCards.map((apartmentCard) => {
+      let name = apartmentCard.querySelector('.listing-name').innerText;
+      let address = apartmentCard.querySelector('.listing-contact-info').innerText;
       return {name, address};
     });
-    return appartmentData;
+    return apartmentData;
   })
   .end()
   .then(result => {
-    console.log(result)
+    fs.writeFile('apartmentData.json', JSON.stringify(result, null, "\t"), (error) => {
+      if (error) {
+        console.log('Somthing went wrong:', error)
+      }
+    });
+    console.log('The file has been saved!')
   })
   .catch(error => {
     console.log('Somthing went wrong:', error)
