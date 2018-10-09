dee9551c841b17b9c237292325e0e296e284cd86
Laura Whitaker <laura.k.whitaker@gmail.com>
2018-10-09 17:30:52 -0600

    Add DELETE endpoint for buildings

--- routes/buildings.js
+++ routes/buildings.js
@@ -12,31 +12,43 @@ router.get('/', (req, res) => {
     .then(buildings => res.status(200).json(buildings))
     .catch(err => res.status(500).json({ err }));
 });
+
 // create new building
 router.post('/', (req, res) => {
   const { name, address } = req.body;
-  const newBuilding = { name, address };
   const requiredParams = ['name', 'address'];
 
   requiredParams.map((param) => {
-    if (!newBuilding[param]) {
+    if (!req.body[param]) {
       return res.status(422).send({
         error: `Expected format: { name: <String>, address: <String> }. You're missing a "${param}" property.`,
       });
     }
+    return param;
   });
 
   database('buildings')
-    .where('name', newBuilding.name)
+    .where('name', name)
     .then((response) => {
       if (response.length > 0) {
         return res.status(409).send({ error: 'That building already exists.' });
       }
       return database('buildings')
-        .insert(newBuilding, 'id')
+        .insert({ name, address }, 'id')
         .then(building => res.status(201).json({ id: building[0] }))
         .catch(err => res.status(500).json({ err }));
     })
+    .catch(err => res.status(500).json({ err }));
+});
+
+// delete building
+router.delete('/:building_id', (req, res) => {
+  const { building_id } = req.params;
+
+  database('buildings')
+    .where('id', building_id)
+    .del()
+    .then(() => res.status(204))
     .catch(err => res.status(500).json({ err }));
 });
 
