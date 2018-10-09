76b5e09a20bac2bfc990507127dd939733cccb3e
Gray Smith <gray.smith00@gmail.com>
2018-10-08 20:34:04 -0600

    Run table migration for buildings, users, interests, and userInterests

--- db/migrations/20181008185614_initial.js
+++ db/migrations/20181008185614_initial.js
@@ -1,8 +1,39 @@
-
-exports.up = function(knex, Promise) {
-  
+exports.up = function (knex, Promise) {
+  return Promise.all([
+    knex.schema.createTable('buildings', (table) => {
+      table.increments('id').primary();
+      table.string('name');
+      table.string('address');
+      table.timestamps(true, true);
+    }),
+    knex.schema.createTable('users', (table) => {
+      table.increments('id').primary();
+      table.string('name');
+      table.integer('building_id').unsigned();
+      table.foreign('building_id');
+      table.timestamps(true, true);
+    }),
+    knex.schema.createTable('interests', (table) => {
+      table.increments('id').primary();
+      table.string('name');
+      table.timestamps(true, true);
+    }),
+    knex.schema.createTable('userInterests', (table) => {
+      table.increments('id').primary();
+      table.integer('user_id').unsigned();
+      table.foreign('user_id');
+      table.integer('interest_id').unsigned();
+      table.foreign('interest_id');
+      table.timestamps(true, true);
+    }),
+  ]);
 };
 
-exports.down = function(knex, Promise) {
-  
+exports.down = function (knex, Promise) {
+  return Promise.all([
+    knex.schema.dropTable('buildings'),
+    knex.schema.dropTable('users'),
+    knex.schema.dropTable('interests'),
+    knex.schema.dropTable('userInterests'),
+  ]);
 };
