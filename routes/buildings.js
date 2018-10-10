3594cd83e91c9dff6e7e7056e6574a88fae45fd4
Laura Whitaker <laura.k.whitaker@gmail.com>
2018-10-09 19:50:11 -0600

    Update DELETE to check building id exists

--- routes/buildings.js
+++ routes/buildings.js
@@ -72,8 +72,16 @@ router.delete('/:building_id', (req, res) => {
 
   database('buildings')
     .where('id', building_id)
-    .del()
-    .then(() => res.status(204))
+    .select()
+    .then((building) => {
+      if (building.length) {
+        database('buildings').where('id', building_id).del()
+          .then(() => res.status(204).send(`Building ${building_id} was successfully deleted.`))
+          .catch(err => res.status(500).json({ err }));
+      } else {
+        return res.status(404).json({ error: `Could not find building with id ${building_id}`});
+      }
+    })
     .catch(err => res.status(500).json({ err }));
 });
 
