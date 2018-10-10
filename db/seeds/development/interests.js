609adcc719c76432fde35d0b09edcc4e20d59976
Gray Smith <gray.smith00@gmail.com>
2018-10-09 20:36:34 -0600

    Create interests seed file

---
+++ db/seeds/development/interests.js
@@ -0,0 +1,7 @@
+const interestData = require('../../../interestData.json');
+
+exports.seed = function (knex) {
+  return knex('interests').del()
+    .then(() => knex('interests').insert(interestData))
+    .catch(error => console.log(`Error seeding data: ${error}`));
+};
