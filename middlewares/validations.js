f7297f4b8ec8060e362b0df74403d79185f46bfb
Gray Smith <gray.smith00@gmail.com>
2018-10-11 17:13:35 -0600

    Build out validateUserParams middleware

--- middlewares/validations.js
+++ middlewares/validations.js
@@ -1,12 +1,12 @@
 exports.validateUserParams = (req, res, next) => {
   const requiredParams = ['name', 'email', 'building_id', 'password'];
 
-  requiredParams.forEach((param) => {
-    if (!req.body[param]) {
-      return res.status(422).json({
-        error: `Expected format: { name: <String>, email: <String>, password: <String>, building_id: <Integer> }. You're missing a "${param}" property.`,
-      });
-    }
-  });
+  const failedParams = requiredParams.filter(param => !req.body[param]);
+
+  if (failedParams.length) {
+    return res.status(422).json({
+      error: `Expected format: { name: <String>, email: <String>, password: <String>, building_id: <Integer> }. You're missing a "${failedParams[0]}" property.`,
+    });
+  }
   next();
 };
